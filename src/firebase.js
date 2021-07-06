afc51f503e3f9300d5e1aea718f0bcaff4b48bb5
NeilS99 <smaugvader99@gmail.com>
2021-07-06 14:27:13 +0530

    Icons for main page

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCKelfzAI04WQ9Wj9nkhCfxb7Mst683Gyk",
+  authDomain: "whatsappclone-2e2c6.firebaseapp.com",
+  projectId: "whatsappclone-2e2c6",
+  storageBucket: "whatsappclone-2e2c6.appspot.com",
+  messagingSenderId: "514859452869",
+  appId: "1:514859452869:web:2f781905822f60277523ac",
+};
