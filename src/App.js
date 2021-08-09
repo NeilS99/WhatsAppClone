fc5c2ea9d7ed2ec271e77a618b6a6e7c5287b09c
NeilS99 <smaugvader99@gmail.com>
2021-08-09 18:20:22 +0530

    Login functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Chat from "./Chat";
+import Login from "./Login";
 import Sidebar from "./Sidebar";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     //BEM
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Sidebar />
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Chat />
-            </Route>
-          </Switch>
-        </Router>
-        {/*Chat*/}
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+          {/*Chat*/}
+        </div>
+      )}
     </div>
   );
 }
